424264146c8013f1a218132eddd77ebc9593df4f
Mohammad Shafi <shafi.bahrami.2015@gmail.com>
2023-03-24 17:21:42 +0100

    Styling calendar page

--- frontend/src/pages/Calendar.js
+++ frontend/src/pages/Calendar.js
@@ -13,10 +13,30 @@ import startOfWeek from "date-fns/startOfWeek";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
-
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import {
+  VStack,
+  FormControl,
+  Input,
+  Button,
+  Text,
+  Box,
+  StackDivider
+} from '@chakra-ui/react';
+import axios from "axios";
+import { ChatState } from "../contexts/ChatContext";
 const DragAndDropCalendar = withDragAndDrop(Calendar);
 
-const CalendarComp=()=>{
+const CalendarComp = () => {
+  const [newEvent, setNewEvent] = useState({
+    title: '',
+    start: '',
+    end: '',
+    chatId: '',
+    users: []
+  });
+  const {selectedChat}=ChatState();
   const locales = {
     "en-US": require("date-fns/locale/en-US")
   };
@@ -49,25 +69,14 @@ const CalendarComp=()=>{
   const [myEvents, setMyEvents] = useState(events);
 
   const moveEvent = useCallback(
-    ({ event, start, end, isAllDay: droppedOnAllDaySlot = false }) => {
-      const { allDay } = event;
+    ({ event, start, end}) => {
+/*       const { allDay } = event;
       if (!allDay && droppedOnAllDaySlot) {
         event.allDay = true;
       }
-
+ */
       setMyEvents((prev) => {
-        const existing = prev.find((ev) => ev.id === event.id) ?? {};
-        const filtered = prev.filter((ev) => ev.id !== event.id);
-        return [...filtered, { ...existing, start, end, allDay }];
-      });
-    },
-    [setMyEvents]
-  );
-
-  const resizeEvent = useCallback(
-    ({ event, start, end }) => {
-      setMyEvents((prev) => {
-        const existing = prev.find((ev) => ev.id === event.id) ?? {};
+        const existing = prev.find((ev) => ev.id === event.id);
         const filtered = prev.filter((ev) => ev.id !== event.id);
         return [...filtered, { ...existing, start, end }];
       });
@@ -75,10 +84,51 @@ const CalendarComp=()=>{
     [setMyEvents]
   );
 
-  const defaultDate = useMemo(() => new Date(2023, 3, 12), []);
+  const resizeEvent = ({ event, start, end }) => {
+      setMyEvents((prev) => {
+        const existing = prev.find((ev) => ev.id === event.id);
+        const filtered = prev.filter((ev) => ev.id !== event.id);
+        return [...filtered, { ...existing, start, end }];
+      });
+    }
+
+
+  const defaultDate = useMemo(() => new Date(), []);
+
+  const submitEvent = async () => {
+    try {
+       setMyEvents(preVal=>[...preVal,newEvent])
+    } catch (err) {
+       console.log(err);
+    }  
+  }
 
   return (
-    <div className="height600">
+    <VStack
+    divider={<StackDivider borderColor="blackAlpha.400" />}
+    spacing={4}
+    >
+    <Text 
+       fontSize="lg" 
+       padding="5px"
+       borderRadius="5px"
+       textAlign="center"
+       marginTop="10px"
+       style={{
+          backgroundColor:"white"
+       }}
+    >
+      Showing calender for chat: {`${selectedChat.groupChatName}`}
+    </Text>
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      gap="100px"
+      alignItems="center"
+      w="100%"
+      p="100px"
+      height="700px"
+    >
       <DragAndDropCalendar
         defaultDate={defaultDate}
         defaultView={Views.MONTH}
@@ -88,8 +138,57 @@ const CalendarComp=()=>{
         onEventResize={resizeEvent}
         popup
         resizable
+        style={{
+          backgroundColor: 'white',
+          borderRadius: '5px',
+          height: '100%',
+          width: '700px',
+          boxShadow: '5px 10px #888888'
+        }}
       />
-    </div>
+      <FormControl
+        style={{
+          backgroundColor: 'white'
+        }}
+        padding="10px"
+        width="35%"
+      >
+        <Input
+          type="text"
+          onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
+          placeholder="Add event title"
+          marginBottom="10px"
+        />
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          gap="20px"
+          marginX="auto"
+          border="1px"
+          borderColor="blackAlpha.400"
+          marginBottom="10px"
+          borderRadius="5px"
+        >
+          <DatePicker
+            selected={newEvent.start}
+            onChange={(start) => setNewEvent({ ...newEvent, start })}
+            placeholderText="Start date"
+          />
+          <DatePicker
+            selected={newEvent.end}
+            onChange={(end) => setNewEvent({ ...newEvent, end })}
+            placeholderText="End date"
+          />
+        </Box>
+        <Button
+          onClick={submitEvent}
+          colorScheme='teal'
+        >
+          Add
+        </Button>
+      </FormControl>
+    </Box>
+    </VStack>
   );
 }
 
